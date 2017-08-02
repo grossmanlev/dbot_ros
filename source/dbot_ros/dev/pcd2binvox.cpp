c6a6c5c9267497b21ed3581890f16549b8576253
grossmanlev <grossmanlev@gmail.com>
2017-08-02 13:37:50 -0400

    added rviz default file and launch file for ease of use

--- source/dbot_ros/dev/pcd2binvox.cpp
+++ source/dbot_ros/dev/pcd2binvox.cpp
@@ -20,7 +20,6 @@
 #include <iomanip> // setprecision
 #include <stdlib.h>
 #include <mutex>
-
 #include <cstdlib>
 #include <cstring>
 
@@ -60,25 +59,6 @@ struct Voxel
   Voxel() : x(0), y(0), z(0) {};
   Voxel(const unsigned int _x, const unsigned int _y, const unsigned int _z) : x(_x), y(_y), z(_z) {};
 };
-
-/*
-// For debugging: Write the voxel indices to a file
-void writeVoxelsToFile(const std::vector<Voxel>& voxels, const std::string& filename)
-{
-  std::ofstream* output = new std::ofstream(filename, std::ios::out);
-  if (!output->good())
-  {
-    std::cerr << "Error: Could not open output file " << output << "! \n" << std::endl;
-    exit(1);
-  }
-  for (size_t i = 0; i < voxels.size(); ++i)
-  {
-    // Write in order (X,Z,Y)
-    *output << voxels.at(i).x << " "  << voxels.at(i).z << " "  << voxels.at(i).y << "\n";
-  }
-  output->close();
-}
-*/
 
 template <typename PointT>
 const bool loadPointCloud(const std::string& file_path,
@@ -430,7 +410,6 @@ int main(int argc, char **argv)
 
   // Create a ROS subscriber for the input point cloud
   ros::Subscriber sub = nh.subscribe ("out_cloud", 1, cloud_cb);
-
   ros::Subscriber sub_state = nh.subscribe("particle_tracker/object_state", 1, state_cb);
 
   // Spin
@@ -438,158 +417,3 @@ int main(int argc, char **argv)
 
   return 0;
 }
-
-
-// int main(int argc, char **argv)
-// {
-//   if (argc < 4)
-//   {
-//     pcl::console::print_error("Syntax is: %s -d <voxel_grid_size [32 to 1024]> input.pcd output.binvox \n", argv[0]);
-//     return -1;
-//   }
-
-//   // Parse the command line arguments for .pcd and .ply files
-//   std::vector<int> pcd_file_indices = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
-//   std::vector<int> binvox_file_indices = pcl::console::parse_file_extension_argument(argc, argv, ".binvox");
-//   if (pcd_file_indices.size() != 1 || binvox_file_indices.size() != 1)
-//   {
-//     pcl::console::print_error("Need one input PCD file and one output Binvox file. \n");
-//     return -1;
-//   }
-
-//   // In binvox, default is 256, max 1024
-//   uint voxel_grid_size;
-//   pcl::console::parse_argument(argc, argv, "-d", voxel_grid_size);
-//   std::cout << "Voxel grid size: " << voxel_grid_size << std::endl;
-
-//   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
-//   const std::string input_file(argv[pcd_file_indices[0]]);
-//   if (!loadPointCloud<pcl::PointXYZ>(input_file, *cloud))
-//   {
-//     return -1;
-//   }
-
-//   pcl::PointXYZ min_point;
-//   pcl::PointXYZ max_point;
-//   pcl::getMinMax3D(*cloud, min_point, max_point);
-
-//   // Calculate the scale factor so the longest side of the volume
-//   // is split into the desired number of voxels
-//   const float x_range = max_point.x - min_point.x;
-//   const float y_range = max_point.y - min_point.y;
-//   const float z_range = max_point.z - min_point.z;
-
-//   const float max_cloud_extent = std::max(std::max(x_range, y_range), z_range);
-//   const float voxel_size = max_cloud_extent / (static_cast<float>(voxel_grid_size) - 1.0);
-//   std::cout << "voxel_size = " << voxel_size << std::endl;
-
-//   const float scale = (static_cast<float>(voxel_grid_size) * max_cloud_extent) / (static_cast<float>(voxel_grid_size) - 1.0);
-
-//   std::cout << "Bounding box: "
-//             << "[" << min_point.x << ", " << min_point.y << ", " << min_point.z << "] - "
-//             << "[" << max_point.x << ", " << max_point.y << ", " << max_point.z << "]" << std::endl;
-
-//   // Calculate the PointCloud's translation from the origin.
-//   // We need to subtract half the voxel size, because points
-//   // are located in the center of the voxel grid. 
-//   float tx = min_point.x - voxel_size / 2.0;
-//   float ty = min_point.y - voxel_size / 2.0;
-//   float tz = min_point.z - voxel_size / 2.0;
-//   // Hack, change -0.0 to 0.0
-//   const float epsilon = 0.0000001;
-//   if ((tx > -epsilon) && (tx < 0.0))
-//   {
-//     tx = -1.0 * tx;
-//   }
-//   if ((ty > -epsilon) && (ty < 0.0))
-//   {
-//     ty = -1.0 * ty;
-//   }
-//   if ((tz > -epsilon) && (tz < 0.0))
-//   {
-//     tz = -1.0 * tz;
-//   }
-//   const pcl::PointXYZ translate(tx, ty, tz);
-//   std::cout << "Normalization transform: (1) translate ["
-//             << formatFloat(translate.x) << ", " << formatFloat(translate.y) << ", " << formatFloat(translate.z) << "], " << std::endl;
-//   std::cout << "                         (2) scale " << scale << std::endl;
-
-//   const unsigned int num_voxels = voxel_grid_size * voxel_grid_size * voxel_grid_size;
-
-//   // Voxelize the PointCloud into a linear array
-//   boost::dynamic_bitset<> voxels_bitset(num_voxels);
-//   for (pcl::PointCloud<pcl::PointXYZ>::iterator it = cloud->begin(); it != cloud->end(); ++it)
-//   {
-//     const Voxel voxel = getGridIndex(*it, translate, voxel_grid_size, scale);
-//     const unsigned int idx = getLinearIndex(voxel, voxel_grid_size);
-//     voxels_bitset[idx] = 1;
-//   }
-
-//   /*
-//   // For debugging: Write voxel indices to a file
-//   std::vector<Voxel> voxels; // for debugging
-//   for (size_t i = 0; i < voxels_bitset.size(); ++i)
-//   {
-//     if (voxels_bitset[i] == 1)
-//     {
-//       const int voxel_grid_width = voxel_grid_size;
-//       const int voxel_grid_height = voxel_grid_size;
-//       const int idx = static_cast<int>(i);
-//       const float ix = static_cast<float>(idx / (voxel_grid_width * voxel_grid_height));
-//       const float iy = static_cast<float>(idx % voxel_grid_size);
-//       const float iz = static_cast<float>((idx / voxel_grid_width) % voxel_grid_height);
-//       voxels.push_back( Voxel(ix, iy, iz) );
-//     }
-//   }
-//   writeVoxelsToFile(voxels, "voxels_from_pcd.txt");
-//   */
-
-//   const std::string output_file(argv[binvox_file_indices[0]]);
-//   std::ofstream* output = new std::ofstream(output_file, std::ios::out | std::ios::binary);
-//   if (!output->good())
-//   {
-//     std::cerr << "Error: Could not open output file " << output << "!" << std::endl;
-//     exit(1);
-//   }
-
-//   // Write the binvox file using run-length encoding
-//   // where each pair of bytes is of the format (run value, run length)
-//   *output << "#binvox 1\n";
-//   *output << "dim " << voxel_grid_size << " " << voxel_grid_size << " " << voxel_grid_size << "\n";
-//   *output << "translate " << formatFloat(translate.x) << " " << formatFloat(translate.y) << " " << formatFloat(translate.z) << "\n";
-//   *output << "scale " << scale << "\n";
-//   *output << "data\n";
-//   unsigned int run_value = voxels_bitset[0];
-//   unsigned int run_length = 0;
-//   for (size_t i = 0; i < num_voxels; ++i)
-//   {
-//     if (voxels_bitset[i] == run_value)
-//     {
-//       // This is a run (repeated bit value)
-//       run_length++;
-//       if (run_length == 255)
-//       {
-//         *output << static_cast<char>(run_value);
-//         *output << static_cast<char>(run_length);
-//         run_length = 0;
-//       }
-//     }
-//     else
-//     {
-//       // End of a run
-//       *output << static_cast<char>(run_value);
-//       *output << static_cast<char>(run_length);
-//       run_value = voxels_bitset[i];
-//       run_length = 1;
-//     }
-//   }
-//   if (run_length > 0)
-//   {
-//     *output << static_cast<char>(run_value);
-//     *output << static_cast<char>(run_length);
-//   }
-//   output->close();
-
-//   std::cout << "done" << std::endl << std::endl;
-//   return 0;
-// }
--- source/dbot_ros/dev/pcl_pipeline.cpp
+++ source/dbot_ros/dev/pcl_pipeline.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <ros/package.h>
+
 #include <mutex>
 #include <math.h>
 #include <iostream>
@@ -16,8 +18,6 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/crop_box.h>
-#include <pcl/filters/crop_hull.h>
-#include <pcl/surface/convex_hull.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/Vertices.h>
 
@@ -35,7 +35,6 @@
 #include <visualization_msgs/InteractiveMarkerInit.h>
 #include <visualization_msgs/InteractiveMarker.h>
 
-#define BOUNDING_SIZE 0.25
 #define PI 3.14159265
 
 ros::Publisher pub, bb_pub, mesh_pub;
@@ -47,24 +46,6 @@ bool goodPos = false;
 Eigen::Vector4f minPoint, maxPoint;
 
 Eigen::Affine3f cropAffine;
-
-// bool getBoundingCylinderOfMesh(std::string mesh_file, shapes::Shape &mesh, bodies::BoundingCylinder &cyl) // adapted from ROS user Benjamin Cohen
-// {
-//   //mesh_file = "package://object_meshes/object_models/oil_bottle.obj";
-//   //if (!mesh_file.empty())
-//   //  return false;
-    
-//   shapes::Shape* tmp = shapes::createMeshFromResource("package://object_meshes/object_models/oil_bottle.obj");
-//   mesh = *tmp;
-//   tmp->print();
-//   const bodies::ConvexMesh *cm = new bodies::ConvexMesh(tmp);
-//   bodies::BoundingCylinder bc;
-//   cm->computeBoundingCylinder(bc);
-//   ROS_INFO("Bounding sphere has radius: %0.3f and length %0.3f\n", bc.radius, bc.length);
-//   cyl = bc;
-//   printf("HERE\n");
-//   return true;
-// }
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
@@ -78,47 +59,6 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 
   // Convert to PCL data type
   pcl_conversions::toPCL(*cloud_msg, *cloud);
-
-  // if(goodPos) {
-  //   pcl::CropHull<pcl::PointXYZ> cropHullFilter;
-  //   pcl::PointCloud<pcl::PointXYZ>::Ptr hullCloud (new pcl::PointCloud<pcl::PointXYZ>);
-  //   pcl::PointCloud<pcl::PointXYZ>::Ptr hullPoints (new pcl::PointCloud<pcl::PointXYZ>);
-  //   std::vector<pcl::Vertices> hullPolygons;
-  //   pcl::PointXYZ p;
-  //   pcl::PointCloud<pcl::PointXYZ>::Ptr hullFiltered (new pcl::PointCloud<pcl::PointXYZ>);
-
-  //   // Convert to PCL data type
-  //   pcl_conversions::toPCL(*cloud_msg, *cloud);
-
-  //   hullCloud->clear();
-  //   // Create sphere points
-  //   printf("Before sphere\n");
-  //   for(float phi = 0.; phi < PI; phi += PI/5.) {
-  //     for(float theta = 0.; theta < 2.0*PI; theta += PI/5.) {
-  //       p.x = radius * cos(theta) * sin(phi) + x_pos;
-  //       p.y = radius * sin(theta) * sin(phi) + y_pos;
-  //       p.z = radius * cos(phi) + z_pos;
-  //       hullCloud->push_back(p);
-  //     }
-  //   }
-  //   printf("After sphere\n");
-  //   pcl::ConvexHull<pcl::PointXYZ> cHull;
-  //   cHull.setInputCloud(hullCloud);
-  //   cHull.reconstruct(*hullPoints, hullPolygons);
-  //   printf("After reconstruct\n");
-
-  //   cropHullFilter.setHullIndices(hullPolygons);
-  //   cropHullFilter.setHullCloud(hullPoints);
-
-  //   pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud (new pcl::PointCloud<pcl::PointXYZ>);
-  //   pcl::fromPCLPointCloud2(*cloud, *inputCloud);
-  //   cropHullFilter.setInputCloud(inputCloud);
-  //   cropHullFilter.filter(*hullFiltered);
-  //   printf("After filtered\n");
-  //   pcl::toPCLPointCloud2(*hullFiltered, *cloud_filtered);
-  // }
-
-
 
   if(goodPos) {
     // Set up the bouning box for the Crop filter
@@ -154,7 +94,6 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     sor.filter (cloud_filtered2);
   }
 
-
   // Convert to ROS data type
   sensor_msgs::PointCloud2 out_cloud;
   pcl_conversions::fromPCL(cloud_filtered2, out_cloud); //moveFromPCL call faster?
@@ -166,8 +105,6 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 void state_cb (const dbot_ros_msgs::ObjectState& state_msg)
 {
   std::lock_guard<std::mutex> lock(pos_mutex);
-  //ROS_INFO("Got a state message!\n");
-  //printf("Received pose %f, %f, %f\n", state_msg.pose.pose.position.x, state_msg.pose.pose.position.y, state_msg.pose.pose.position.z);
   x_pos = state_msg.pose.pose.position.x;
   y_pos = state_msg.pose.pose.position.y;
   z_pos = state_msg.pose.pose.position.z;
@@ -188,72 +125,24 @@ void model_cb (const visualization_msgs::Marker& model_msg)
 {
   if (model_msg.type == model_msg.MESH_RESOURCE) {
     shapes::Shape* mesh = shapes::createMeshFromResource("package://object_meshes/object_models/oil_bottle.obj");
-    //mesh->print();
     const bodies::ConvexMesh* cm = new bodies::ConvexMesh(mesh);
     bodies::BoundingCylinder cyl;
     cm->computeBoundingCylinder(cyl);
-    //ROS_INFO("Bounding sphere has radius: %0.3f and length %0.3f\n", cyl.radius, cyl.length);
     radius = cyl.length / 2.0;
-
 
     Eigen::Quaternionf og_rotation(original_pose.orientation.w, original_pose.orientation.x, 
                                    original_pose.orientation.y, original_pose.orientation.z); //NOTE: Eigen::Quaternion takes w,x,y,z
     Eigen::Quaternionf m_rotation(model_msg.pose.orientation.w, model_msg.pose.orientation.x, 
                                   model_msg.pose.orientation.y, model_msg.pose.orientation.z);
     Eigen::Quaternionf composed = og_rotation * m_rotation;
-    // Eigen::Quaternionf q1 = og_rotation; Eigen::Quaternionf q2 = m_rotation;
-    // composed.setIdentity();
-
-    // composed.w() = q1.w() * q2.w() - q1.vec().dot(q2.vec());
-    // composed.vec() = q1.w() * q2.vec() + q2.w() * q1.vec() + q1.vec().cross(q2.vec());
-
-    //printf("Original Pose: %f, %f, %f, %f\n", og_rotation.x(), og_rotation.y(), og_rotation.z(), og_rotation.w());
-    //printf("Marker Pose: %f, %f, %f, %f\n", m_rotation.x(), m_rotation.y(), m_rotation.z(), m_rotation.w());
-
-    //Eigen::Quaterniond quat(cyl.pose.rotation());
-    //Eigen::Translation<double, 3> trans(cyl.pose.translation());
     Eigen::Vector3f trans(model_msg.pose.position.x, model_msg.pose.position.y, model_msg.pose.position.z);
-
     Eigen::Affine3f crop = Eigen::Affine3f::Identity();
-    //Eigen::Translation3f(trans) * Eigen::AngleAxisf(composed) * Eigen::Scaling(0.1);
     crop.translation() = trans;
     crop.linear() = composed.toRotationMatrix(); // * Eigen::Scaling(radius)
 
     pos_mutex.lock();
     cropAffine = crop;
     pos_mutex.unlock();
-    //crop.linear() = composed.toRotationMatrix() * Eigen::Scaling(radius);
-    //crop.translation() = trans;
-
-
-    // visualization_msgs::Marker marker;
-    // marker.header.frame_id = "/camera_depth_optical_frame"; //SPECIFIC (hardcoded)
-    // marker.header.stamp = ros::Time::now();
-    // marker.ns = "bounding_box";
-    // marker.id = 0;
-    // marker.type = visualization_msgs::Marker::SPHERE;
-    // marker.action = visualization_msgs::Marker::ADD;
-
-    // printf("Pose: %f, %f, %f\n", model_msg.pose.position.x, model_msg.pose.position.y, model_msg.pose.position.z);
-    // marker.pose.position.x = model_msg.pose.position.x;
-    // marker.pose.position.y = model_msg.pose.position.y;
-    // marker.pose.position.z = model_msg.pose.position.z;
-    // marker.pose.orientation.x = composed.x();
-    // marker.pose.orientation.y = composed.y();
-    // marker.pose.orientation.z = composed.z();
-    // marker.pose.orientation.w = composed.w();
-
-    // marker.scale.x = cyl.length;
-    // marker.scale.y = cyl.length;
-    // marker.scale.z = cyl.length;
-    // marker.color.r = 0.0f;
-    // marker.color.g = 0.0f;
-    // marker.color.b = 1.0f;
-    // marker.color.a = 1.0;
-
-    // marker.lifetime = ros::Duration();
-    // bb_pub.publish(marker);
-
 
     // Publish the mesh reconstruction marker
     visualization_msgs::Marker mesh_marker;
@@ -285,7 +174,9 @@ void model_cb (const visualization_msgs::Marker& model_msg)
     std::string stl_file = "recon_0.stl";
     DIR* dir;
     struct dirent* ent;
-    if((dir = opendir("src/dbot_getting_started/object_meshes/object_models")) != NULL) {
+    std::string path = ros::package::getPath("dbot_ros");
+    path += "/../dbot_getting_started/object_meshes/object_models";
+    if((dir = opendir(&path[0])) != NULL) {
       while((ent = readdir(dir)) != NULL) {
         std::string file (ent->d_name);
         //std::cout << file << std::endl;
@@ -308,7 +199,6 @@ void model_cb (const visualization_msgs::Marker& model_msg)
     std::ostringstream oss;
     oss << "package://object_meshes/object_models/" << stl_file;
     mesh_marker.mesh_resource = oss.str();
-    //recon_stl_num = (recon_stl_num + 1) % 2;
     mesh_pub.publish(mesh_marker); //publish the mesh 
   }
 }
@@ -322,20 +212,14 @@ int main (int argc, char** argv)
 
   x_pos = y_pos = z_pos = 0.0;
 
-  // Create a ROS subscriber for the input point cloud
+  // Create a ROS subscribers
   ros::Subscriber sub = nh.subscribe ("camera/depth/points", 1, cloud_cb);
-
-  // Create a ROS subscriber for the object_state publisher
   ros::Subscriber sub_state = nh.subscribe("particle_tracker/object_state", 1, state_cb);
-
   ros::Subscriber sub_intmodel = nh.subscribe("interactive_marker_initializer/update_full", 1, intmodel_cb);
   ros::Subscriber sub_model = nh.subscribe("particle_tracker/object_model", 1, model_cb);
 
-  // Create a ROS publisher for the output point cloud
+  // Create a ROS publisher for the output point cloud and reconstructed mesh
   pub = nh.advertise<sensor_msgs::PointCloud2> ("out_cloud", 1);
-
-  //bb_pub = nh.advertise<visualization_msgs::Marker> ("bounding_box", 1);
-
   mesh_pub = nh.advertise<visualization_msgs::Marker> ("recon_mesh", 1);
 
   // Spin
